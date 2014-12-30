0bf1260dcc1f8d86be80b216c10db118eac34835
daver182 <daver182@gmail.com>
2014-12-30 09:30:58 -0300

    Commit inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name seedApp
+ * @description
+ * # seedApp
+ *
+ * Main module of the application.
+ */
+angular
+	.module('seedApp', [
+		'ngAnimate',
+		'ngMessages',
+		'ngResource',
+		'ngSanitize',
+		'ngTouch',
+		'ui.router',
+		'satellizer'
+	])
+	.config(function ($stateProvider, $urlRouterProvider, $authProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: '/views/home.html',
+				controller: 'HomeCtrl'
+			})
+	});
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seedApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the seedApp
+ */
+angular.module('seedApp')
+	.controller('HomeCtrl', function ($scope, $http) {
+		$scope.title = 'Home Controller';
+		$http({
+			url: '/test/restricted',
+			method: 'GET'
+		}).then(function(response) {
+			$scope.datos = response.data;
+		}, function(error) {
+			console.log(error.data);
+		});
+	});
